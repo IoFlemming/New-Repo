3458a5bf71a2340ee0c3cb8885446e9a754f8c85
Foli Creppy <folicreppy1@gmail.com>
2016-05-11 16:20:36 -0400

    improved syntax

--- jQuery_Basics.js
+++ jQuery_Basics.js
@@ -1,4 +1,14 @@
-
-$("#h2").click(function(){
-  $("#h2").hide();
+$("#h1").click(function(){
+$("#h1").hide ("#h1").fadeIn(6000);
 })
+$(".para").click(function(){
+$(".para").fadeOut(6000);
+})
+$(".para2").click(function(){
+$(".para2").fadeIn(6000);
+})
+var count = $("h2").find ("*").length;
+alert(count);
+$(".para").text("Behold! I am the Magnificent Jquery! I have decided that Latin is boring and I will change your tags! MWHWHWHWHWHWHAHAHAHAHAHAHAHA!!....but alas, I only have enough magical power to change a couple of tags to English.....OH NO! I'M FADING!!!!AHAHHHHHHHHAHAAAAA!!");
+$("body").css("background", "#FFD700");
+$(".para").css("color", "#fefdfb");
